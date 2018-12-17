bb3b92d0868504c03b7b8ca1cfe149d091e2ca58
Youhou1 <45788792+Youhou1@users.noreply.github.com>
2018-12-17 18:32:56 +0100

    Update Create_bdd_and_insert.sql

--- Genie_Logiciel/Connexion_bdd/Create_bdd_and_insert.sql
+++ Genie_Logiciel/Connexion_bdd/Create_bdd_and_insert.sql
@@ -10,5 +10,5 @@ CREATE DATABASE javadb DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL ON javadb.* TO 'user'@'localhost' IDENTIFIED BY 'password';
 
-CREATE TABLE `javabase`.`tableau_ninja` ( `id` INT NOT NULL AUTO_INCREMENT , `nom` TEXT NOT NULL , `arme` TEXT NOT NULL , `kill` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `javadb`.`tableau_ninja` ( `id` INT NOT NULL AUTO_INCREMENT , `nom` TEXT NOT NULL , `arme` TEXT NOT NULL , `kill` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 INSERT INTO `tableau_ninja` (`id`, `nom`, `arme`, `kill`) VALUES ('', 'naruto', 'shuriken', '0'), ('', 'sasuke', 'sabre', '8');
