bfae52e86fcec6a0f15585b375abf20e46a9ac14
Youhou1 <florian.lm@outlook.com>
2018-12-18 13:42:46 +0100

    update

--- Genie_Logiciel/Connexion_bdd/Create_bdd_and_insert.sql
+++ Genie_Logiciel/Connexion_bdd/Create_bdd_and_insert.sql
@@ -1,6 +1,6 @@
 /*
-database : javabase
-user : java
+database : javadb
+user : user
 pass : password
 */
 
@@ -10,5 +10,11 @@ CREATE DATABASE javadb DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL ON javadb.* TO 'user'@'localhost' IDENTIFIED BY 'password';
 
-CREATE TABLE `javadb`.`tableau_ninja` ( `id` INT NOT NULL AUTO_INCREMENT , `nom` TEXT NOT NULL , `arme` TEXT NOT NULL , `kill` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-INSERT INTO `tableau_ninja` (`id`, `nom`, `arme`, `kill`) VALUES ('', 'naruto', 'shuriken', '0'), ('', 'sasuke', 'sabre', '8');
+CREATE TABLE  users (
+    id INT AUTO_INCREMENT,
+    prenom VARCHAR(50) NOT NULL,
+    nom VARCHAR(50) NOT NULL,
+    date_de_naissance VARCHAR(10) NOT NULL,
+
+    PRIMARY KEY (id));
+INSERT INTO users (prenom, nom, date_de_naissance) VALUES ("Ludovico" ,"Einaudi" ,"23/11/1955"); INSERT INTO users (prenom, nom, date_de_naissance) VALUES ("Frédéric" ,"Chopin" ,"01/03/1810"); INSERT INTO users (prenom, nom, date_de_naissance) VALUES ("Claude" ,"Debussy" ,"22/08/1862");
